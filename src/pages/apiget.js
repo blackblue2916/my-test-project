480d286e47ca91fbcb03afb6ff44a9cfd1778f21
blackblue2916 <dl2008bogu2016@gmail.com>
2022-08-10 09:50:31 +0800

    2208100950

--- src/pages/apiget.js
+++ src/pages/apiget.js
@@ -6,17 +6,24 @@ function API_GET() {
   const [videoDB, setVideoDB] = useState([]);
 
   useEffect(() => {
-    axios
-      .get("https:/api-get-now.herokuapp.com/api-bjloveyou-q1w2e3r4-freevideos")
-      .then((res) => setVideoDB(res.data))
-      .then(console.log(videoDB));
-  }, []);
+    // axios
+    //   .get("https:/api-get-now.herokuapp.com/api-bjloveyou-q1w2e3r4-freevideos")
+    //   .then((res) => setVideoDB(res.data));
+
+    fetch("https:/api-get-now.herokuapp.com/api-bjloveyou-q1w2e3r4-freevideos")
+      .then((res) => res.json())
+      .then((data) => setVideoDB(data));
+  }, [videoDB]);
   return (
     <div>
       {videoDB.length > 0 && (
         <div className="apige-test">
           <p>{videoDB[0].__id__}</p>
-          <img src={videoDB[0].img} />
+          <img src={videoDB[0].img} alt="" />
+          <p>{videoDB[1].__id__}</p>
+          <img src={videoDB[1].img} alt="" />
+          <p>{videoDB[2].__id__}</p>
+          <img src={videoDB[2].img} alt="" />
         </div>
       )}
       {/* {videoDB.length > 0 &&
